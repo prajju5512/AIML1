00d02164cd794d503f70561d6901ae2ae11d5a23
prajju <prajju@gmail.com>
2023-08-21 14:53:50 -0700

    change6

---
+++ pandas2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+data = pd.read_csv("C:/Users/SPTINT-08/Desktop/iris/Iris.csv")
+print(data)
+print(data.head(5))
+print(data.tail(5))
+print(data["SepalLengthCm"])
+print(data.describe())
+print(data["PetalLengthCm"])
+print(data.info())
+print(data.dtypes)
+print(data.count())
